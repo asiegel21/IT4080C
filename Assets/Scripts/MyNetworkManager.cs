9ebdd04d906d16826b325857f82fc8ef77292e72
asiegel21 <acsiegel80@gmail.com>
2021-12-06 19:41:25 -0500

    Commit 2
    Network additions

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,20 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        MyNetworkPlayer player = conn.identity.GetComponent<MyNetworkPlayer>();
+
+        player.SetDisplayName($"Player{numPlayers}");
+
+        Color displayColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+
+        player.SetDisplayColor(displayColor);
+    }
+}
---
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -0,0 +1,43 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MyNetworkPlayer : NetworkBehaviour
+{
+    [SerializeField] private TMP_Text displayNameText = null;
+    [SerializeField] private Renderer displayColorRenderer = null;
+
+    [SyncVar(hook = nameof(HandleDisplayNameUpdated))]
+    [SerializeField]
+    private string displayName = "Missing Name";
+
+    [SyncVar(hook= nameof(HandleDisplayColorUpdated))]
+    [SerializeField]
+    private Color displayColor = Color.black;
+
+    [Server]
+    public void SetDisplayName(string newDisplayName)
+    {
+        displayName = newDisplayName;
+    }
+
+    [Server]
+    public void SetDisplayColor(Color newDisplayColor)
+    {
+        displayColor = newDisplayColor;
+    }
+
+    private void HandleDisplayNameUpdated(string oldName, string newName)
+    {
+        displayNameText.text = newName;
+    }
+
+    private void HandleDisplayColorUpdated(Color oldColor, Color newColor)
+    {
+        displayColorRenderer.material.SetColor("_BaseColor", newColor);
+    }
+
+
+}
