8c2b710cbbdfce3a1405c5a9925c1ab3fee3016d
asiegel21 <acsiegel80@gmail.com>
2021-12-08 02:03:38 -0500

    Building Commit
    building scripts

---
+++ Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Health : NetworkBehaviour
+{
+    [SerializeField] private int maxHealth = 100;
+
+    [SyncVar(hook = nameof(HandleHealthUpdated))]
+    private int currentHealth;
+
+    public event Action ServerOnDie;
+
+    public event Action<int, int> ClientOnHealthUpdated;
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        currentHealth = maxHealth;
+
+        UnitBase.ServerOnPlayerDie += ServerHandlePlayerDie;
+    }
+
+    public override void OnStopServer()
+    {
+        UnitBase.ServerOnPlayerDie -= ServerHandlePlayerDie;
+    }
+
+    [Server]
+    private void ServerHandlePlayerDie(int connectionId)
+    {
+        if(connectionToClient.connectionId != connectionId) { return; }
+
+        DealDamage(currentHealth);
+    }
+
+    [Server]
+    public void DealDamage(int damgeAmount)
+    {
+        if (currentHealth == 0) { return; }
+
+        currentHealth = Mathf.Max(currentHealth - damgeAmount, 0);
+
+        if(currentHealth != 0) { return; }
+
+        ServerOnDie?.Invoke();
+
+        Debug.Log("We Died!");
+    }
+
+
+    #endregion
+
+    #region Client
+
+    private void HandleHealthUpdated(int oldHealth, int newHealth)
+    {
+        ClientOnHealthUpdated?.Invoke(newHealth, maxHealth);
+    }
+
+
+    #endregion
+
+
+}
---
+++ Assets/Scripts/Units/UnitMovement.cs
@@ -0,0 +1,79 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitMovement : NetworkBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent agent = null;
+
+    [SerializeField]
+    private Targeter targeter = null;
+
+    [SerializeField]
+    private float chaseRange = 10f;
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        GameOverHandler.ServerOnGameOver += ServerHandleGameOver;
+    }
+
+    public override void OnStopServer()
+    {
+        GameOverHandler.ServerOnGameOver -= ServerHandleGameOver;
+    }
+
+    [ServerCallback]
+    private void Update()
+    {
+        if(targeter.GetTarget() != null)
+        {
+            Targetable target = targeter.GetTarget();
+
+
+            if((target.transform.position - transform.position).sqrMagnitude > chaseRange * chaseRange)
+            {
+                agent.SetDestination(targeter.GetTarget().transform.position);
+            }
+            else if(agent.hasPath)
+            {
+                agent.ResetPath();
+            }
+
+            return;
+        }
+
+
+        if (!agent.hasPath) { return; }
+
+        if(agent.remainingDistance > agent.stoppingDistance) { return; }
+
+        agent.ResetPath();
+    }
+
+    [Command]
+    public void CmdMove(Vector3 position)
+    {
+        targeter.ClearTarget();
+
+        if(!NavMesh.SamplePosition(position, out NavMeshHit hit, 1f, NavMesh.AllAreas)) { return; }
+
+        agent.SetDestination(hit.position);
+    }
+
+    [Server]
+    private void ServerHandleGameOver()
+    {
+        agent.ResetPath();
+    }
+
+    #endregion
+
+
+
+
+}
